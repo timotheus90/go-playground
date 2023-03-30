619ebb0699f035dff7d19688656422367dc5e18c
Timo Gerth <timo.gerth@rewe-digital.com>
2023-03-30 10:50:58 +0200

    copy && setup cleaning-task pet project

---
+++ README.md
@@ -0,0 +1,20 @@
+# install dependencies
+```
+go get
+```
+
+# Update dependencies
+```
+go get -u ./...
+go mod tidy -v
+```
+
+# Intellij
+Enable the go modules integration:
+
+Preferences > GO > GO Modules.
+
+# Run application
+```
+go run main.go
+```
---
+++ main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/labstack/echo/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type CleaningTask struct {
+	ID          uint           `gorm:"primarykey" json:"id"`
+	CreatedAt   time.Time      `json:"-"`
+	UpdatedAt   time.Time      `json:"-"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
+	Description string         `json:"description"`
+	DueDate     time.Time      `json:"dueDate"`
+	Assignee    string         `json:"assignee"`
+	Completed   bool           `json:"completed"`
+	Category    TaskCategory   `json:"category"`
+}
+
+type TaskCategory string
+
+const (
+	CategoryKitchen TaskCategory = "kitchen"
+	CategoryBaths   TaskCategory = "baths"
+	CategoryFloors  TaskCategory = "floors"
+	CategoryOther   TaskCategory = "other"
+)
+
+func main() {
+	// init database
+	dataSourceName := "host=localhost user=postgres password=postgres port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dataSourceName), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+		Logger:                                   logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// enable debug logging
+
+	// declare models
+	err = db.AutoMigrate(&CleaningTask{})
+	if err != nil {
+		panic(err)
+	}
+
+	e := echo.New()
+	cleaningTasksPath := "/api/cleaning-tasks"
+	e.GET(cleaningTasksPath, func(c echo.Context) error {
+		cleaningTasks := []CleaningTask{{}}
+		result := db.Model(&CleaningTask{}).Find(&cleaningTasks)
+		if result.Error != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+		}
+		return c.JSON(http.StatusOK, cleaningTasks)
+	})
+
+	e.POST(cleaningTasksPath, func(c echo.Context) error {
+		cleaningTask := CleaningTask{}
+		err := json.NewDecoder(c.Request().Body).Decode(&cleaningTask)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err)
+		}
+
+		// create cleaning task in db
+		result := db.Create(&cleaningTask)
+
+		if result.Error != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+		}
+
+		return c.JSON(http.StatusOK, cleaningTask)
+	})
+
+	e.GET(cleaningTasksPath+"/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		cleaningTask := CleaningTask{}
+		result := db.Model(CleaningTask{}).First(&cleaningTask, id)
+		if result.Error != nil {
+			if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+				return echo.NewHTTPError(http.StatusNotFound)
+			}
+
+			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+		}
+
+		return c.JSON(http.StatusOK, cleaningTask)
+	})
+
+	e.PUT(cleaningTasksPath+"/:id", func(c echo.Context) error {
+		cleaningTask := CleaningTask{}
+		err := json.NewDecoder(c.Request().Body).Decode(&cleaningTask)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err)
+		}
+
+		id, _ := strconv.Atoi(c.Param("id"))
+		result := db.First(&CleaningTask{}, id)
+		if result.RowsAffected == 0 {
+			return c.NoContent(http.StatusNotFound)
+		}
+		if result.Error != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+		}
+
+		// save updated cleaning task in db
+		cleaningTask.ID = uint(id)
+		// TODO: include id in request
+		result = db.Save(&cleaningTask)
+		if result.Error != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+		}
+
+		return c.JSON(http.StatusOK, cleaningTask)
+	})
+
+	e.DELETE(cleaningTasksPath+"/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		result := db.Find(&CleaningTask{}, id)
+		if result.RowsAffected == 0 {
+			return c.NoContent(http.StatusNotFound)
+		}
+		if result.Error != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+		}
+
+		result = db.Delete(&CleaningTask{}, id)
+		if result.Error != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+		}
+
+		return c.NoContent(http.StatusNoContent)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
