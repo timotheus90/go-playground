d50709cef15ef46d3ec98fed0a8371f384537851
Timo Gerth <timo.gerth@rewe-digital.com>
2023-03-30 16:31:00 +0200

    refactor: extract API Handler methods

--- main.go
+++ main.go
@@ -38,6 +38,92 @@ var (
 	err error
 )
 
+func getCleaningTasks(c echo.Context) error {
+	cleaningTasks := []CleaningTask{{}}
+	result := db.Model(&CleaningTask{}).Find(&cleaningTasks)
+	if result.Error != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+	}
+	return c.JSON(http.StatusOK, cleaningTasks)
+}
+
+func createCleaningTask(c echo.Context) error {
+	cleaningTask := CleaningTask{}
+	err := json.NewDecoder(c.Request().Body).Decode(&cleaningTask)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	// create cleaning task in db
+	result := db.Create(&cleaningTask)
+
+	if result.Error != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+	}
+
+	return c.JSON(http.StatusOK, cleaningTask)
+}
+
+func getCleaningTaskById(c echo.Context) error {
+	id := c.Param("id")
+	cleaningTask := CleaningTask{}
+	result := db.Model(CleaningTask{}).First(&cleaningTask, id)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return echo.NewHTTPError(http.StatusNotFound)
+		}
+
+		return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+	}
+
+	return c.JSON(http.StatusOK, cleaningTask)
+}
+
+func updateCleaningTaskById(c echo.Context) error {
+	cleaningTask := CleaningTask{}
+	err := json.NewDecoder(c.Request().Body).Decode(&cleaningTask)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	id, _ := strconv.Atoi(c.Param("id"))
+	result := db.First(&CleaningTask{}, id)
+	if result.RowsAffected == 0 {
+		return c.NoContent(http.StatusNotFound)
+	}
+	if result.Error != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+	}
+
+	// save updated cleaning task in db
+	cleaningTask.ID = uint(id)
+	// TODO: include id in request
+	result = db.Save(&cleaningTask)
+	if result.Error != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+	}
+
+	return c.JSON(http.StatusOK, cleaningTask)
+}
+
+func deleteCleaningTaskById(c echo.Context) error {
+	id := c.Param("id")
+	result := db.Find(&CleaningTask{}, id)
+	if result.RowsAffected == 0 {
+		return c.NoContent(http.StatusNotFound)
+	}
+	if result.Error != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+	}
+
+	result = db.Delete(&CleaningTask{}, id)
+	if result.Error != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
+	}
+
+	return c.NoContent(http.StatusNoContent)
+}
+
 func main() {
 	// init database
 	dataSourceName := "host=localhost user=postgres password=postgres port=5432 sslmode=disable"
@@ -58,92 +144,13 @@ func main() {
 	}
 
 	e := echo.New()
+
 	cleaningTasksPath := "/api/cleaning-tasks"
-	e.GET(cleaningTasksPath, func(c echo.Context) error {
-		cleaningTasks := []CleaningTask{{}}
-		result := db.Model(&CleaningTask{}).Find(&cleaningTasks)
-		if result.Error != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
-		}
-		return c.JSON(http.StatusOK, cleaningTasks)
-	})
-
-	e.POST(cleaningTasksPath, func(c echo.Context) error {
-		cleaningTask := CleaningTask{}
-		err := json.NewDecoder(c.Request().Body).Decode(&cleaningTask)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, err)
-		}
-
-		// create cleaning task in db
-		result := db.Create(&cleaningTask)
-
-		if result.Error != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
-		}
-
-		return c.JSON(http.StatusOK, cleaningTask)
-	})
-
-	e.GET(cleaningTasksPath+"/:id", func(c echo.Context) error {
-		id := c.Param("id")
-		cleaningTask := CleaningTask{}
-		result := db.Model(CleaningTask{}).First(&cleaningTask, id)
-		if result.Error != nil {
-			if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-				return echo.NewHTTPError(http.StatusNotFound)
-			}
-
-			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
-		}
-
-		return c.JSON(http.StatusOK, cleaningTask)
-	})
-
-	e.PUT(cleaningTasksPath+"/:id", func(c echo.Context) error {
-		cleaningTask := CleaningTask{}
-		err := json.NewDecoder(c.Request().Body).Decode(&cleaningTask)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, err)
-		}
-
-		id, _ := strconv.Atoi(c.Param("id"))
-		result := db.First(&CleaningTask{}, id)
-		if result.RowsAffected == 0 {
-			return c.NoContent(http.StatusNotFound)
-		}
-		if result.Error != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
-		}
-
-		// save updated cleaning task in db
-		cleaningTask.ID = uint(id)
-		// TODO: include id in request
-		result = db.Save(&cleaningTask)
-		if result.Error != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
-		}
-
-		return c.JSON(http.StatusOK, cleaningTask)
-	})
-
-	e.DELETE(cleaningTasksPath+"/:id", func(c echo.Context) error {
-		id := c.Param("id")
-		result := db.Find(&CleaningTask{}, id)
-		if result.RowsAffected == 0 {
-			return c.NoContent(http.StatusNotFound)
-		}
-		if result.Error != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
-		}
-
-		result = db.Delete(&CleaningTask{}, id)
-		if result.Error != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, result.Error)
-		}
-
-		return c.NoContent(http.StatusNoContent)
-	})
+	e.GET(cleaningTasksPath, getCleaningTasks)
+	e.POST(cleaningTasksPath, createCleaningTask)
+	e.GET(cleaningTasksPath+"/:id", getCleaningTaskById)
+	e.PUT(cleaningTasksPath+"/:id", updateCleaningTaskById)
+	e.DELETE(cleaningTasksPath+"/:id", deleteCleaningTaskById)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
